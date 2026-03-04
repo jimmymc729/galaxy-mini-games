1070abe4d4d972c798f8fc73b04a3478782ebecc
jimmymc729 <jimmymc89@gmail.com>
2026-03-04 18:39:16 -0500

    Add files via upload

--- js/site-shell.js
+++ js/site-shell.js
@@ -1,4 +1,6 @@
 (function () {
+  const GA_MEASUREMENT_ID = 'G-ZCHTLPM0KL';
+
   const navItems = [
     { id: 'games', label: 'Games', href: 'index.html' },
     { id: 'updates', label: 'Updates', href: 'updates.html' },
@@ -46,8 +48,46 @@
       '</div>';
   }
 
+  function initAnalytics() {
+    if (!GA_MEASUREMENT_ID || window.location.protocol === 'file:') {
+      return;
+    }
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = window.gtag || function () {
+      window.dataLayer.push(arguments);
+    };
+
+    if (!window.__GMG_GA_LOADER_ADDED) {
+      const loader = document.createElement('script');
+      loader.async = true;
+      loader.src =
+        'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(GA_MEASUREMENT_ID);
+      loader.dataset.gmgGaLoader = '1';
+      document.head.appendChild(loader);
+      window.__GMG_GA_LOADER_ADDED = true;
+    }
+
+    if (!window.__GMG_GA_CONFIGURED) {
+      window.gtag('js', new Date());
+      window.gtag('config', GA_MEASUREMENT_ID, { anonymize_ip: true });
+      window.__GMG_GA_CONFIGURED = true;
+    }
+
+    window.GMGAnalytics = window.GMGAnalytics || {
+      track: function (eventName, eventParams) {
+        if (!eventName || typeof window.gtag !== 'function') {
+          return;
+        }
+
+        window.gtag('event', eventName, eventParams || {});
+      }
+    };
+  }
+
   function initShell() {
     const pageId = document.body ? document.body.dataset.page : '';
+    initAnalytics();
     renderHeader(pageId);
     renderFooter();
   }
